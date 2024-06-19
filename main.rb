9d5162705b0e77ebbf3d52e1bb2ca7c2eb72103f
chandan gupta <94867034+chandan-devs@users.noreply.github.com>
2024-06-19 10:26:04 +0000

    Initial commit

---
+++ main.rb
@@ -0,0 +1,8 @@
+puts 'Hello, World!'
+address = [1,2,3,4,5,6,7,8,9]
+new_address = address.reverse
+print new_address
+print address
+new_address = address.reverse!
+print new_address
+print address
